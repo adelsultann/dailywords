3ff44818a1ed73a3bc30403a3339614926d0ed74
adelsultann <maax.1992@gmail.com>
2024-10-26 20:25:31 +0300

    adding the backend server

--- src/Pages/contactForm.js
+++ src/Pages/contactForm.js
@@ -23,7 +23,7 @@ const ContactPage = () => {
     setIsSubmitting(true);
 
     try {
-      const response = await fetch('http://localhost:5000/api/contact', {
+      const response = await fetch('https://dailywords-backend.onrender.com/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
